176a881673ed0bcd9dad9e07b4de33b8dd3fc754
Tieppo <88698431+JTieppo@users.noreply.github.com>
2024-03-09 01:49:05 -0300

    .

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,14 @@
-export default function Login(){
-    return(
-        <div>
-            login...
+export default function Login() {
+    return (
+        <div className="flex min-h-screen flex-col items-center ">
+            <img src="/login-desktop.jpeg" className="hidden lg:block min-h-screen max-h-screen min-w-full absolute" alt="" />
+            <img src="/login-mobile.jpeg" className="lg:hidden min-h-screen max-h-screen min-w-full absolute" alt="" />
+            <div className="flex flex-col z-10 w-full items-center my-auto bg-[#38383861] rounded-xl max-w-80 p-8">
+                <h1 className="text-green-200 text-4xl mb-10">Login</h1>
+                <input className="text-black p-2 px-4 rounded-full w-64" type="mail" />
+                <input className="mt-4 text-black p-2 px-4 rounded-full w-64" type="password" />
+                <button className="mt-4 bg-green-200 text-black p-2 px-4 rounded-full w-64" >Login</button>
+            </div>
         </div>
     )
 }
