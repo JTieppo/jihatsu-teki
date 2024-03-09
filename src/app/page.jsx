2603867d00a9642e0cc748e4c23e1789a8121528
Tieppo <88698431+JTieppo@users.noreply.github.com>
2024-03-09 01:05:09 -0300

    .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,25 +4,25 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center ">
-            <img src="/inicio.webp" className="min-h-screen max-h-screen min-w-full absolute" alt="" />
+            <img src="/inicio-desktop.jpeg" className="hidden lg:block min-h-screen max-h-screen min-w-full absolute" alt="" />
+            <img src="/inicio-mobile.jpeg" className="lg:hidden min-h-screen max-h-screen min-w-full absolute" alt="" />
             <div className="flex z-10 w-full items-center">
-                <div className="flex flex-col w-full p-10 px-40 items-center">
-                    <nav className="flex justify-between w-full">
-                        <Link href={''} className="cursor-pointer">Link1</Link>
-                        <Link href={''} className="cursor-pointer">Link2</Link>
-                        <img src="/glove.svg" className="w-14 " alt="" />
-                        <Link href={''}>Link3</Link>
-                        <Link href={'/signup'}>Signup</Link>
-
+                <div className="flex flex-col w-full p-10 lg:px-40 items-center">
+                    <nav className="flex justify-between w-full text-black">
+                        <Link href={'/sobre'} className="cursor-pointer">Sobre</Link>
+                        <Link href={'/'} className="cursor-pointer">Link2</Link>
+                        <img src="/glove-white.svg" className="w-6" alt="" />
+                        <Link href={'/login'} className='cursor-pointerc' >Login</Link>
+                        <Link href={'/signup'} className='cursor-pointer'>Signup</Link>
                     </nav>
                     <div className="flex flex-row mt-20 text-black">
-                        <input className="rounded-full h-12 w-96 mx-auto pl-6 pr-12 focus:outline-none" type="text" />
+                        <input className="rounded-full h-12 w-72 lg:w-96 mx-auto -ml-1 pl-6 pr-12 focus:outline-none" type="text" />
                         <button className=" bg-emerald-200 -ml-11 my-auto rounded-full p-2"><MagnifyingGlassIcon className="w-6"></MagnifyingGlassIcon></button>
                     </div>
-                    <div className="p-2 px-4 w-80 mt-4 flex justify-between bg-white text-black rounded-full text-sm">
-                        <Link href={'/voluntario'}>Se Voluntariar</Link>
+                    <div className="w-72 lg:w-80 mt-4 flex justify-between bg-white text-black rounded-full text-[12px] lg:text-sm">
+                        <Link href={'/voluntario'} className='p-2 px-4 cursor-pointer'>Se Voluntariar</Link>
                         <div className="border"></div>
-                        <Link href={'/projetista'}>Iniciar  serviço</Link>
+                        <Link href={'/projetista'} className='p-2 px-4 cursor-pointer'>Iniciar  serviço</Link>
                     </div>
                 </div>
             </div>
