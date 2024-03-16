37333876c8c0b13085cbdc7b477f3af5002d9aec
Tieppo <88698431+JTieppo@users.noreply.github.com>
2024-03-15 22:49:29 -0300

    .

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,7 +1,6 @@
 import {NextResponse} from 'next/server'
 
 export async function GET() {
-    const dados = await req.json();
-    console.log(dados);
-    return NextResponse.json({dados})
+
+    return NextResponse.json({message: "funcionando"})
 }
