8db34333d95c6d87075349b07b2396c085fedfbb
LucasCatuyama <lucas.catuyama@outlook.com>
2024-05-27 14:59:00 -0300

    day3 home footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -56,7 +56,9 @@ export default function Home() {
               TRANSFORME SUA <br />
               VONTADE EM AÇÃO E <br />
               PARTICIPE DA{" "}
-              <span className="border-b-4 border-green-700 italic">MUDANÇA!</span>
+              <span className="border-b-4 border-green-700 italic">
+                MUDANÇA!
+              </span>
             </h1>
             <p className="text-sm text-[#D0D0D0] leading-[19.6px] tracking-[1.12px]">
               A Jihatsu-teki conecta ações de limpeza comunitárias e voluntários{" "}
@@ -80,41 +82,89 @@ export default function Home() {
           <div class="bg-white opacity-15 border border-dashed border-[#009F66] p-4"></div>
         </section>
       </section>
-        {/* Carrossel da home */}
-        <section>
+      {/* Carrossel da home */}
+      <section className="bg-[#001012]">
+        <div>
+          <div>
+            <p>ENCONTRE UMA AÇÃO PERTO DE VOCÊ</p>
+          </div>
+          <div>
+            <p>Procurar cidade</p>
+            <button>Ver tudo</button>
+          </div>
+        </div>
 
-        </section>
-
-        {/* Sobre a Jihatsu-teki da home */}
-        <section className="bg-[#001012]">
+        <div>
+          <div>
+            <img src="" alt="" />
+            <h2>Limpeza do Hospital Universiário</h2>
             <div>
-                <div>
-                    <p>ENCONTRE UMA AÇÃO PERTO DE VOCÊ</p>
-                </div>
-                <div>
-                    <p>Procurar cidade</p>
-                    <button>Ver tudo</button>
-                </div>
+              <img src="" alt="" />
+              <p>Dia 20/06/2024 às 17:00</p>
+              <p>Av. Mandacaru, 1590 - Parque das Laranjeiras, Maringa - PR</p>
             </div>
-            
-            <div>
-                <div>
-                    <img src="" alt="" />
-                    <h2>Limpeza do Hospital Universiário</h2>
-                    <div>
-                        <img src="" alt="" />
-                        <p>Dia 20/06/2024 às 17:00</p>
-                        <p>Av. Mandacaru, 1590 - Parque das Laranjeiras, Maringa - PR</p>
-                    </div>
-                    <div>
+            <div></div>
+            <p>Mais detalhes</p>
+          </div>
+        </div>
+      </section>
 
-                    </div>
-                    <p>Mais detalhes</p>
-                </div>
+      {/* Sobre a Jihatsu-teki da home */}
+      <section className="flex flex-row justify-center px-48 py-16 gap-4 bg-[#001b12] border-t-2 border-green-700">
+
+        {/* Card esquerdo */}
+        <div className="w-[440px] h-auto flex flex-col gap-4">
+
+          <div className="w-full h-2/6 flex flex-row justify-start items-center gap-3 p-6 bg-card-gradient rounded-2xl border border-[#008A6533]">
+            <div className="flex justify-center items-center w-10 h-10 bg-[#009F66] rounded-full p-2.5">
+              <p className="text-2xl/normal font-extrabold">自</p>
             </div>
+            <div className="">
+              <p className="text-white text-base/[20.8px] font-extrabold italic">自 (ji)</p>
+              <p className="text-white text-xs/[15.6px] font-light italic">significa "auto" ou "próprio"</p>
+            </div>
+          </div>
 
-        </section>
-      
+          <div className="w-full h-2/6 flex flex-row justify-start items-center gap-3 p-6 bg-card-gradient rounded-2xl border border-[#008A6533]">
+            <div className="flex justify-center items-center w-10 h-10 bg-[#009F66] rounded-full p-2.5">
+              <p className="text-2xl/normal font-extrabold">発</p>
+            </div>
+            <div className="">
+              <p className="text-white text-base/[20.8px] font-extrabold italic">発 (hatsu)</p>
+              <p className="text-white text-xs/[15.6px] font-light italic">significa "partir", "iniciar" ou "emitir"</p>
+            </div>
+          </div>
+
+          <div className="w-full h-2/6 flex flex-row justify-start items-center gap-3 p-6 bg-card-gradient rounded-2xl border border-[#008A6533]">
+            <div className="flex justify-center items-center w-10 h-10 bg-[#009F66] rounded-full p-2.5">
+              <p className="text-2xl/normal font-extrabold">的</p>
+            </div>
+            <div className="">
+              <p className="text-white text-base/[20.8px] font-extrabold italic">的 (teki)</p>
+              <p className="text-white text-xs/[15.6px] font-light italic">significa "partir", "iniciar" ou "emitir"</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Card direito */}
+        <div className="max-w-[440px] h-auto flex flex-col gap-6 p-6 bg-card-gradient rounded-2xl border border-[#008A6533]">
+          <div className="">
+            <p className="bg-white-gradient white-gradient text-2xl/none font-bold">自発的</p>
+            <p className="bg-white-gradient white-gradient text-6xl/none font-black ">Jihatsu-teki</p>
+          </div>
+          <div>
+            <p className="text-white text-xs/normal font-light leading-[15.6px] italic">Algo feito de manaira espontânea ou voluntária</p>
+          </div>
+          <div className="text-[#D0D0D0] text-sm/normal font-light leading-[16.8px] tracking-[0.98px]">
+            A jihatsu-teki surgiu com o objetivo de pipipi popopo e tal coisa e
+            nao sei o que lá A jihatsu-teki surgiu com o objetivo de pipipi
+            popopo e tal coisa e nao sei o que láA jihatsu-teki surgiu com o
+            objetivo de pipipi popopo e tal coisa e nao sei o que láA
+            jihatsu-teki surgiu com o objetivo de pipipi popopo e tal coisa e
+            nao sei o que lá
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,22 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "card-gradient": 'linear-gradient(99deg, rgba(0, 64, 47, 0.20) 5.57%, rgba(0, 138, 101, 0.20) 97.4%)',
+        "card-green":'rgba(0, 157, 61, 0.40)',
+        "white-gradient": 'linear-gradient(93deg, rgba(255, 255, 255, 0.90) 1.99%, #D0D0D0 30.64%, rgba(208, 208, 208, 0.60) 100.27%)'
+
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.white-gradient': {
+          'background-clip': 'text',
+          '-webkit-background-clip': 'text',
+          'color': 'transparent',
+        },
+      });
+    },
+  ],
 };
