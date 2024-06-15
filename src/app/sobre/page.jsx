51104f81fbf42915122effe24e7224b26bb9e19a
Lucas <67424170+LucasCatuyama@users.noreply.github.com>
2024-06-15 10:12:57 -0300

    dev sobre lucas

--- src/app/sobre/page.jsx
+++ src/app/sobre/page.jsx
@@ -1,8 +1,58 @@
-"use client"
+"use client";
+import Link from "next/link";
+
 export default function Sobre() {
-    return (
-        <div className="flex min-h-screen flex-col items-center ">
-            página do sobre
+  return (
+    <div className="bg-card-gradient min-h-screen">
+      {/*header que pode ser um component */}
+      <header className="flex flex-row justify-between items-center px-16 w-full h-[86px]">
+        <div className="flex gap-3">
+          <img
+            className="w-[47px] h-[50px]"
+            src="assets/images/logo.png"
+            alt="Logo da Jihatsu-teki"
+          />
+          <div className="flex">
+            <div className="flex flex-col">
+              <Link href={"/"} className="text-base font-bold text-darker-white">
+                自発的
+              </Link>
+              <Link
+                href={"/"}
+                className="text-2xl font-extrabold text-darker-white"
+              >
+                Jihatsu-teki
+              </Link>
+            </div>
+          </div>
         </div>
-    )
+        <div className="flex gap-4">
+          <button className="px-6 py-3 border border-white rounded-xl text-xs/[14.4px] tracking-[1.44px]">
+            User name
+          </button>
+          <button className="px-6 py-3 border border-white rounded-xl text-xs/[14.4px] tracking-[1.44px]">
+            Meus projetos
+          </button>
+          <Link
+            href={"/"}
+            className="px-6 py-3 text-xs/[14.4px] tracking-[1.44px]"
+          >
+            Sair
+          </Link>
+        </div>
+      </header>
+
+      <div className="bg-gradient-to-r h-[1px] from-[#ffffff00] via-green-600 to-[#ffffff00]"></div>
+
+      <main className="flex flex-row mt-3">
+        <nav className="flex flex-col border-r-4 border-solid border-gradient-right">
+          <Link href={"/"} className="hover:bg-custom-gradient-hover px-4 py-2">Meus projetos</Link>
+          <Link href={"/"} className="hover:bg-custom-gradient-hover px-4 py-2">Instituições</Link>
+          <Link href={"/"} className="hover:bg-custom-gradient-hover px-4 py-2">Dados pessoais</Link>
+        </nav>
+
+        
+      </main>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,15 @@ module.exports = {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         "card-gradient": 'linear-gradient(99deg, rgba(0, 64, 47, 0.20) 5.57%, rgba(0, 138, 101, 0.20) 97.4%)',
-        "card-green":'rgba(0, 157, 61, 0.40)',
-        "white-gradient": 'linear-gradient(93deg, rgba(255, 255, 255, 0.90) 1.99%, #D0D0D0 30.64%, rgba(208, 208, 208, 0.60) 100.27%)'
-
+        "card-green": 'rgba(0, 157, 61, 0.40)',
+        "white-gradient": 'linear-gradient(93deg, rgba(255, 255, 255, 0.90) 1.99%, #D0D0D0 30.64%, rgba(208, 208, 208, 0.60) 100.27%)',
+        'custom-gradient-hover': 'linear-gradient(90deg, #00402F 10%, #009f66 80%)',
+      },
+      colors: {
+        "darker-white": '#D0D0D0',
+        'custom-green': '#009F66',
       },
     },
   },
@@ -25,6 +29,11 @@ module.exports = {
           'background-clip': 'text',
           '-webkit-background-clip': 'text',
           'color': 'transparent',
+        },
+        '.border-gradient-right': {
+          'border-right-width': '1px',
+          'border-right-style': 'solid',
+          'border-image': 'linear-gradient(to bottom, #ffffff00, #38a169, #ffffff00) 1 100%',
         },
       });
     },
