31efd16ff2fc62d972cc98ffa218028cda0b3304
Misael Correia <misaelccorreia@gmail.com>
2024-06-15 19:03:50 -0300

    estilizacao parcial

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -33,30 +33,36 @@ export default function Login() {
                         setSenhaEmailInvalidos(false)
                     }, 3000)
                 }
-            })}
+            })
     }
 
 
     return (
         <div className='h-screen'>
-            {/*AQUI VAI UM COMPONENTE*/}
             <Navbar/>
-            {/*
-            
-            <div className="flex">
-                <form className="flex flex-col w-96 mx-auto absolute" onSubmit={Login}>
-                    <h1 className="text-4xl mx-auto mb-4">Login</h1>
-                    <label className='bg-black' htmlFor="email">Email</label>
-                    <input className="text-black" name='email' type="mail" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                    <label htmlFor="senha">Senha</label>
-                    <input className="text-black" name='senha' type="password" value={senha} onChange={(e) => setSenha(e.target.value)} required />
-                    <input type="submit" className="" value={'Login'} />
+            <div className="flex justify-center h-full flex-row m-top">
+                <form className="flex flex-col w-96 absolute mx-auto my-auto" onSubmit={Login}>
+                    <h1 className="text-4xl text-darker-white  mx-auto mb-4">Fazer login</h1>
+                    <h3 className="text-sm	 mx-auto mb-2">Crie uma conta para se inscrever em projetos</h3>
+                    {/* <label className='' htmlFor="email">Email</label> */}
+                    <input className="text-white bg-transparent	border border-neutral-500 rounded-lg p-3 my-4" placeholder='Email' name='email' type="mail" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                    {/* border image não funcionou, precisa corrigir */}
+                    {/* <label htmlFor="senha">Senha</label> */}
+                    <input className="text-white bg-transparent	border border-neutral-500 rounded-lg p-3 my-4" placeholder='Senha' name='senha' type="password" value={senha} onChange={(e) => setSenha(e.target.value)} required />
+                    {/* border image não funcionou, precisa corrigir */}
+                    <div className='flex justify-around	'>
+                    <a href="" className='w-full text-center my-auto'>Recuperar senha</a>
+                    <input className="rounded-lg bg-button-green p-2 px-6 w-full" type="submit" value={'Entrar'} />
+                    </div>
+                    <div id='separador' className='w-96	h-px border-gradient-right'></div>
+                    {/* bg-teal-500  */}
+                    <a href="" className='text-xs mt-6 text-center text-neutral-500'>Ainda não tem conta? Cadastre-se aqui!</a>
                 </form>
-                <div className={`transition-opacity duration-700 ease-in-out ${senhaEmailInvalidos ? 'opacity-100 pointer-events-none' : 'opacity-0 pointer-events-none'}`}>
-                    <p className='mb-20 bg-red-950 p-4 rounded-lg'>Email ou senha inválidos</p>
-                </div>
+                {/* <div className={` fixed transition-opacity duration-700 ease-in-out ${senhaEmailInvalidos ? 'opacity-100 pointer-events-none' : 'opacity-0 pointer-events-none'}`}> */}
+                    {/* <p className='mb-20 bg-red-950 p-4 rounded-lg'>Email ou senha inválidos</p> */}
+                {/* </div> */}
             </div>
-            */}
+            
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,15 @@ module.exports = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         "card-gradient": 'linear-gradient(99deg, rgba(0, 64, 47, 0.20) 5.57%, rgba(0, 138, 101, 0.20) 97.4%)',
-        "card-green":'rgba(0, 157, 61, 0.40)',
-        "white-gradient": 'linear-gradient(93deg, rgba(255, 255, 255, 0.90) 1.99%, #D0D0D0 30.64%, rgba(208, 208, 208, 0.60) 100.27%)'
-
+        "card-green": 'rgba(0, 157, 61, 0.40)',
+        "white-gradient": 'linear-gradient(93deg, rgba(255, 255, 255, 0.90) 1.99%, #D0D0D0 30.64%, rgba(208, 208, 208, 0.60) 100.27%)',
+        "custom-gradient-hover": 'linear-gradient(90deg, #00402F 10%, #009f66 80%)',
+      },
+      colors: {
+        "darker-white": '#D0D0D0',
+        "button-green": '#009F66',
       },
     },
   },
@@ -25,6 +28,16 @@ module.exports = {
           'background-clip': 'text',
           '-webkit-background-clip': 'text',
           'color': 'transparent',
+        },
+        '.border-gradient-right': {
+          'border-right-width': '1px',
+          'border-right-style': 'solid',
+          'border-image': 'linear-gradient(to bottom, #ffffff00, #38a169, #ffffff00) 1 100%',
+        },
+        '.border-gradient-left': {
+          'border-left-width': '1px',
+          'border-left-style': 'solid',
+          'border-image': 'linear-gradient(to bottom, #ffffff00, #38a169, #ffffff00) 1 100%',
         },
       });
     },
