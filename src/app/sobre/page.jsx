6c2ef402924b7b0c53aa4a3bdc13d39cc8bc6f53
Lucas <67424170+LucasCatuyama@users.noreply.github.com>
2024-06-15 11:46:26 -0300

    meus projetos done

--- src/app/sobre/page.jsx
+++ src/app/sobre/page.jsx
@@ -14,7 +14,10 @@ export default function Sobre() {
           />
           <div className="flex">
             <div className="flex flex-col">
-              <Link href={"/"} className="text-base font-bold text-darker-white">
+              <Link
+                href={"/"}
+                className="text-base font-bold text-darker-white"
+              >
                 自発的
               </Link>
               <Link
@@ -44,14 +47,59 @@ export default function Sobre() {
 
       <div className="bg-gradient-to-r h-[1px] from-[#ffffff00] via-green-600 to-[#ffffff00]"></div>
 
-      <main className="flex flex-row mt-3">
-        <nav className="flex flex-col border-r-4 border-solid border-gradient-right">
-          <Link href={"/"} className="hover:bg-custom-gradient-hover px-4 py-2">Meus projetos</Link>
-          <Link href={"/"} className="hover:bg-custom-gradient-hover px-4 py-2">Instituições</Link>
-          <Link href={"/"} className="hover:bg-custom-gradient-hover px-4 py-2">Dados pessoais</Link>
+      <main className="flex flex-row px-16 py-8">
+        <nav className="flex flex-col gap-2">
+          <Link
+            href={"/"}
+            className="w-40 pr-4 py-2 text-right hover:bg-custom-gradient-hover"
+          >
+            Meus projetos
+          </Link>
+          <Link
+            href={"/"}
+            className="w-40 pr-4 py-2 text-right hover:bg-custom-gradient-hover"
+          >
+            Instituições
+          </Link>
+          <Link
+            href={"/"}
+            className="w-40 pr-4 py-2 text-right hover:bg-custom-gradient-hover"
+          >
+            Dados pessoais
+          </Link>
         </nav>
 
-        
+        <section className="flex flex-col gap-4 w-full pl-6 border-l-1 border-solid border-gradient-left">
+          <div className="flex flex-row justify-between">
+            <div>
+              <h1 className="text-2xl font-black">Meus projetos</h1>
+            </div>
+            <div className="flex flex-row gap-[10px]">
+              <button className="w-48 h-10  border border-white rounded-xl">
+                Procurar projetos
+              </button>
+              <button className="w-48 h-10  bg-button-green rounded-xl">
+                Criar projeto
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-8 p-16 bg-[#012b22] rounded-xl">
+            <div className="">
+              <p>
+                Você ainda não está participando de nenhum projeto, escolha uma
+                opção abaixo para começar:{" "}
+              </p>
+            </div>
+            <div className="flex flex-row gap-4">
+              <button className="w-48 h-10  border border-white rounded-xl">
+                Quero ser voluntário
+              </button>
+              <button className="w-48 h-10  bg-button-green rounded-xl">
+                Criar projeto
+              </button>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
