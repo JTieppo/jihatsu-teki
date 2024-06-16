721c42ca2743f835df54af2500ff3487d4094adf
Lucas <67424170+LucasCatuyama@users.noreply.github.com>
2024-06-16 11:29:32 -0300

    instituicao done

--- src/app/login/usuario/[id]/institution/page.jsx
+++ src/app/login/usuario/[id]/institution/page.jsx
@@ -1,8 +1,32 @@
 export default function Projects() {
-    return(
-        <div className="h-full p-2 pt-16">
-            <h1 className="text-xl">Instituições</h1>
-            
-        </div>
-    )
-};
+  return (
+    <div className="flex flex-row  w-full">
+      <main className="flex flex-row w-full pt-14 pr-16">
+        <section className="flex flex-col gap-4 w-full">
+          <div className="flex flex-row justify-between">
+            <div>
+              <h1 className="text-2xl font-black">Instituição</h1>
+            </div>
+            <div className="flex flex-row gap-[10px]">
+              <button className="w-48 h-10  bg-button-green rounded-xl">
+                Criar instituição
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-8 p-16 bg-[#012b22] rounded-xl">
+            <div className="">
+              <p>
+              Para cadastrar um projeto ou uma ação, você precisa ter uma instituição cadastrada.{" "}
+              </p>
+            </div>
+            <div className="flex flex-row gap-4">
+              <button className="w-48 h-10  bg-button-green rounded-xl">
+                Criar instituição
+              </button>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
